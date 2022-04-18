0d7a5e86738cbab2c9946ada101defbee3cf4288
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-04-18 11:13:06 +0200

    almost done but reset function dont work properly

--- app.js
+++ app.js
@@ -19,11 +19,16 @@ function startTheGame(){
     startAndResetButtonElement.textContent = 'stop';
     let totalseconds = 0;
     setInterval(setTime, 1000);
-    ++totalseconds;
-    secondsElement.innerHTML = pad(totalseconds);
-    minutesElement.innerHTML = pad(parseInt(totalseconds/60));
-    hoursElement.innerHTML = pad(parseInt(totalseconds/3600));
+    function setTime(){
+      ++totalseconds;
+      secondsElement.innerHTML = pad(totalseconds) - (minutesElement.innerHTML*60) - (hoursElement.innerHTML*3600);
+      minutesElement.innerHTML = pad(parseInt(totalseconds/60)) - (hoursElement.innerHTML*60);
+      hoursElement.innerHTML = pad(parseInt(totalseconds/3600));      
+    }
   } else if (startAndResetButtonElement.textContent = "stop") {
+    clearInterval()
+    totalseconds = 0;
+    value = 0;
     secondsElement.innerHTML = 0;
     minutesElement.innerHTML = 0;
     hoursElement.innerHTML = 0;
